8f4495f5688cc94c231b76401240a5f4ad316250
Dpbm <dpbm136@gmail.com>
2024-07-25 15:57:01 -0300

    finished last local test

--- backend.py
+++ backend.py
@@ -1,5 +1,5 @@
 from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
-from qiskit_ibm_runtime.fake_provider import FakeCambridgeV2
+from qiskit_ibm_runtime.fake_provider import FakeCambridgeV2, FakeGuadalupeV2
 from qiskit_ibm_runtime import QiskitRuntimeService, SamplerV2
 from dotenv import load_dotenv
 import os
@@ -10,10 +10,14 @@ QiskitRuntimeService.save_account(channel="ibm_quantum", token=os.getenv("IBM_TO
 service = QiskitRuntimeService()
 
 osaka_backend = service.backend("ibm_osaka")
-fake_backend = FakeCambridgeV2()
+fake_backend_cambridge = FakeCambridgeV2()
+fake_backend_guadalupe = FakeGuadalupeV2()
 
 transpiler_real_backend = generate_preset_pass_manager(optimization_level=3, backend=osaka_backend)
 sampler_real_backend = SamplerV2(osaka_backend)
 
-transpiler_fake_backend = generate_preset_pass_manager(optimization_level=3, backend=fake_backend)
-sampler_fake_backend = SamplerV2(fake_backend)
+transpiler_fake_backend_cambridge = generate_preset_pass_manager(optimization_level=3, backend=fake_backend_cambridge)
+sampler_fake_backend_cambridge = SamplerV2(fake_backend_cambridge)
+
+transpiler_fake_backend_guadalupe = generate_preset_pass_manager(optimization_level=3, backend=fake_backend_guadalupe)
+sampler_fake_backend_guadalupe = SamplerV2(fake_backend_guadalupe)
